3748105e7e74aa7b510485bebf7658813dc937b7
eulaly <johnmosescarter@gmail.com>
2026-04-14 01:44:42 -0400

    remove commented code/cleanup

--- src/storage.ts
+++ src/storage.ts
@@ -3,19 +3,6 @@ import { DEFAULT_VERSION_ID } from "./versions";
 const ext = (globalThis as any).browser ?? chrome;
 const KEY = "defaultVersionId";
 
-// export async function getVersion(): Promise<string> {
-//   const data = await chrome.storage.local.get(KEY);
-//   return (data[KEY] as string)
-//    ?? DEFAULT_VERSION_ID;
-// }
-
-// export async function setVersion(version: string): Promise<void> {
-//   await chrome.storage.local.set({
-//     [KEY]: version
-//   });
-// }
-
-
 export async function getVersion(): Promise<string> {
   const data = await ext.storage.local.get(KEY);
   return (data[KEY] as string)
